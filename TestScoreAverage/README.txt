e936491208b2fc6927fe30591886132d26d3c57e
James Mitchell <jmitchell238@gmail.com>
2020-09-29 17:03:30 -0500

    Added Program
    Added TestScoreAverage Program

---
+++ TestScoreAverage/README.txt
@@ -0,0 +1,14 @@
+PROJECT TITLE:			Test Score Average
+
+PURPOSE OF PROJECT:		Assignment for Computer Science Class
+
+VERSION or DATE:		v0.1
+
+HOW TO START THIS PROJECT:	CMD Prompt> File Folder> -$ java TestScoreAverage.java
+
+AUTHORS:			James Mitchell
+
+USER INSTRUCTIONS:		Run the program.
+
+OUTPUT:				The program will display the average of the 3 test scores found
+					in the source code.
---
+++ TestScoreAverage/TestScoreAverage.java
@@ -0,0 +1,10 @@
+//Test Score Average Program
+public class TestScoreAverage
+{
+   public static void main (String [] args)
+      { int test1 = 60, test2 = 90, test3 =100, average; //Creates variables and assigns test scores.
+      average = (test1 + test2 + test3) / 3;             //Formula for Calculating Average
+      
+      System.out.println("The Average Score of your Three test is "+average); //Display Message with Average
+      }
+}
